65dc3b7a25dae1fcc0c2c67a72ae31f10dcfdaaf
Eished <kished@outlook.com>
2021-06-14 21:29:31 +0800

    增加第二三章内容

--- C++Practice/1.test.cpp
+++ C++Practice/1.test.cpp
@@ -1,6 +1,17 @@
-#include <iomanip>
 #include <iostream>
+// #include <stdafx>
+#include <assert.h>
+#include <string.h>
 using namespace std;
 int main()
 {
+  char b[] = "asdfasdf";
+  string c = b;
+  float f = 1.232222222222222222;
+  long double df = 1.232222222223333333333333333333;
+  cout << c << "\n"
+       << b << "\n"
+       << df << "\n"
+       << f << endl;
+  assert(1 != 1);
 }
