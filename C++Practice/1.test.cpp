e6638f61fb00243ebd0347067e3b048ab2d50829
Eished <kished@outlook.com>
2021-06-15 03:51:09 +0800

    第四章基础容器更新

--- C++Practice/1.test.cpp
+++ C++Practice/1.test.cpp
@@ -2,16 +2,59 @@
 // #include <stdafx>
 #include <assert.h>
 #include <string.h>
+#include <vector>
 using namespace std;
 int main()
 {
-  char b[] = "asdfasdf";
-  string c = b;
-  float f = 1.232222222222222222;
-  long double df = 1.232222222223333333333333333333;
-  cout << c << "\n"
-       << b << "\n"
-       << df << "\n"
-       << f << endl;
-  assert(1 != 1);
+  // int a[] = {1, 2, 3, 4};
+  // int len = sizeof(a) / sizeof(a[0]); //得到数组容量
+  // for (int index = 0; index < len; ++index)
+  // {
+  //   if (a[index] == 3)
+  //   {
+  //     cout << index << endl;
+  //     return index;
+  //   }
+  // }
+
+  // int a[2][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+  // for (int row = 0; row < 2; row++)
+  // {
+  //   for (int col = 0; col < 4; col++)
+  //   {
+  //     cout << a[row][col] << " ";
+  //   }
+  //   cout << endl;
+  // }
+  vector<int> vec = {1, 2, 3, 4};
+  // 先查看大小，自动扩容
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " " << vec.capacity() << " " << vec.size() << endl;
+  }
+  cout << "尾部插入" << endl;
+  vec.push_back(5); // 尾部插入
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " " << vec.capacity() << " " << vec.size() << endl;
+  }
+  cout << "尾部删除" << endl;
+  vec.pop_back(); // 从尾部删除
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " " << vec.capacity() << " " << vec.size() << endl;
+  }
+  cout << "指定位置插入" << endl;
+  vec.insert(vec.end() - 1, 6); // 指定位置插入
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " " << vec.capacity() << " " << vec.size() << endl;
+  }
+  cout << "指定位置删除" << endl;
+  vec.erase(vec.end() - 2); // 指定位置删除
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " " << vec.capacity() << " " << vec.size() << endl;
+  }
+  cout << endl;
 }
